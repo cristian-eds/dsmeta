a15c5853ce2d7b60c15c3a1c504f9a6366dc508a
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-11-16 22:42:00 -0300

    Component Notification Button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
-
+import NotificationButton from "./components/NotificationButton/notificationButton"
 
 function App() {
   return (
-    <h1>Olá Mundo</h1>
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
