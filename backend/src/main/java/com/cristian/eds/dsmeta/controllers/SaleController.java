ecad3d4a50226fa128c9798781d97326954d7bc9
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-11-20 21:53:29 -0300

    Alter findSales, add pagging and query to filter min date and max date

--- backend/src/main/java/com/cristian/eds/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/cristian/eds/dsmeta/controllers/SaleController.java
@@ -1,11 +1,12 @@
 package com.cristian.eds.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cristian.eds.dsmeta.entities.Sale;
@@ -19,8 +20,8 @@ public class SaleController {
 	public SaleService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Sale>> getSales() {
-		return ResponseEntity.status(200).body(service.findSales());
+	public ResponseEntity<Page<Sale>> getSales(@RequestParam(defaultValue = "") String minDate,@RequestParam(defaultValue = "") String maxDate, Pageable pageable) {
+		return ResponseEntity.status(200).body(service.findSales(minDate, maxDate, pageable));
 	}
 
 }
