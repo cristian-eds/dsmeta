02a449ae1b54a9a165822b0b2887329e87cbe27e
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-11-21 21:45:46 -0300

    Twilio SMS

--- backend/src/main/java/com/cristian/eds/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/cristian/eds/dsmeta/controllers/SaleController.java
@@ -5,23 +5,34 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cristian.eds.dsmeta.entities.Sale;
 import com.cristian.eds.dsmeta.services.SaleService;
+import com.cristian.eds.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
 	
 	@Autowired
-	public SaleService service;
+	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
+	
 	
 	@GetMapping
 	public ResponseEntity<Page<Sale>> getSales(@RequestParam(defaultValue = "") String minDate,@RequestParam(defaultValue = "") String maxDate, Pageable pageable) {
 		return ResponseEntity.status(200).body(service.findSales(minDate, maxDate, pageable));
 	}
 
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+	}
 }
--- backend/src/main/java/com/cristian/eds/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/cristian/eds/dsmeta/services/SaleService.java
@@ -24,6 +24,7 @@ public class SaleService {
 		
 		LocalDate min = minDate.equals("") ? today.minusDays(365) :LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
 		return repository.findSales(min, max, pageable);
 	}
 }
